4bff6487d63deb38f64e536da3e76250385fd401
doyupK <gltlvl12@gmail.com>
2024-01-04 20:58:18 +0900

    init

---
+++ src/main/java/com/testus/testus/TestusApplication.java
@@ -0,0 +1,13 @@
+package com.testus.testus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testus/testus/TestusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testus.testus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
