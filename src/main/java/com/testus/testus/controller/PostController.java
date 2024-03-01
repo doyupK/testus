6724e30b2b67d53d3c0ff62e2c6cf7f22ec4a27b
doyupK <gltlvl12@gmail.com>
2024-03-02 00:04:51 +0900

    modify security, swagger description

--- src/main/java/com/testus/testus/controller/PostController.java
+++ src/main/java/com/testus/testus/controller/PostController.java
@@ -4,6 +4,8 @@ import com.testus.testus.common.response.ResponseDto;
 import com.testus.testus.dto.post.PostThumbnailDto;
 import com.testus.testus.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +20,14 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/post")
+@Tag(name = "03. 테스트 관련")
 public class PostController {
     private final PostService postService;
 
 
     @GetMapping("/latest")
     @Operation(summary = "최신 테스트 게시글 조회(6개)")
-    public ResponseEntity<ResponseDto<List<PostThumbnailDto>>> getLatestPost(@RequestParam String category) {
+    public ResponseEntity<ResponseDto<List<PostThumbnailDto>>> getLatestPost(@Parameter(example = "APP or SERVICE or GAME") @RequestParam String category) {
         return ResponseEntity
                 .ok()
                 .body(postService.getLatestPost(category));
@@ -32,7 +35,7 @@ public class PostController {
 
     @GetMapping("/popular")
     @Operation(summary = "인기 테스트 게시글 조회(6개)")
-    public ResponseEntity<ResponseDto<List<PostThumbnailDto>>> getPopularPost(@RequestParam String category) {
+    public ResponseEntity<ResponseDto<List<PostThumbnailDto>>> getPopularPost(@Parameter(example = "APP or SERVICE or GAME") @RequestParam String category) {
 
         return ResponseEntity
                 .ok()
